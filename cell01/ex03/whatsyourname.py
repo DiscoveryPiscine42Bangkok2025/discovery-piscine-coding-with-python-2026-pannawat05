a2d90e731b42b49c4204f4c41a6a53111cc68b9b
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2026-02-09 16:26:26 +0700

    ex03

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,5 @@
+first = input("Hey, what's your first name? : ").strip()
+last = input("And your last name?: ").strip()
+
+# ใช้ f-string เพื่อรวมตัวแปร
+print(f"Well, pleased to meet you, {first} {last}")
