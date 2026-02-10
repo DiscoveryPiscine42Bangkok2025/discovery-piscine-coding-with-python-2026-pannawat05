462e9ee26837ebf6641696421344121754e6370a
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2026-02-11 05:14:38 +0700

    cell4-2

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+n1 = int(input('Give me the first number: '))
+n2 = int(input('Give me the second number: '))
+print('Thank you!')
+s = n1+n2
+mi = n1-n2
+ml = n1*n2
+di = n1/n2
+print(f"{n1} +{n2} = {s}")
+print(f"{n1} -{n2} = {mi}")
+print(f"{n1} *{n2} = {ml}")
+print(f"{n1} /{n2} = {di}")
