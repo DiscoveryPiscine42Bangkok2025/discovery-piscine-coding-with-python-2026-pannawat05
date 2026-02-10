f1f622324205e04ef1238b2bfc8a6aeaf7cb25ee
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2026-02-10 08:41:37 +0700

    cell3-00

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+n = int(input('Enter a number less than 25\n'))
+if (n > 25):
+    print('ERROR')
+else:
+    for i in range (n,26):
+        print(f'Inside the loop, my variable is {i}')
