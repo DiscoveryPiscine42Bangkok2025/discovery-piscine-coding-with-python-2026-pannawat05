6afd4c90982ffdbffcb25cd7217d55c8a1d29f11
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2026-02-10 19:44:47 +0700

    cell4-1

---
+++ cell04/ex01/age.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+a = int(input("Please tell me your age: "))
+print(f"You are currently {a} years old.")
+for i in range (10,40,10):
+    print(f"In {i} years, you'll be {a+i} years old.")
