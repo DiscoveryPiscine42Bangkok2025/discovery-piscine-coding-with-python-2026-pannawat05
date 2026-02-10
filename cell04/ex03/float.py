fb61ba93c25eecfe99d2a5227311647967d3b3de
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2026-02-11 05:24:35 +0700

    cell4-3

---
+++ cell04/ex03/float.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+try:
+    user_input = input("Give me a number: ")
+    number = float(user_input)
+
+    if number.is_integer():
+        print("This number is an integer.")
+    else:
+        print("This number is a decimal.")
+except ValueError:
+    print("Invalid input! Please enter a number.")
