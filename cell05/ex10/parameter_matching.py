9a1717c92743353ea684b4edd3774d36223e298b
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2026-02-12 08:08:16 +0700

    cell05-10

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv)  > 1:
+    t = input('What was the parameter? ')
+    if t == sys.argv[1]:
+        print('Good job!')
+    else:
+        print('Nope, sorry...')
+
+else:
+    print('none')
+
+
+
